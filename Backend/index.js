6827be7a98c35ef8eba1e522deeb48c1291f5857
Sneha Menat <snehamenat029@gmail.com>
2024-03-18 01:25:07 +0530

    "change in url"

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ["https://ecommerce-website-using-react-cyan.vercel.app"],
+    origin: ["https://ecommerce-website-using-react-cyan.vercel.app/"],
     methods:["POST", "GET", "DELETE"],
     credentials: true, 
 }));
