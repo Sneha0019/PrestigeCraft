ee87fe079f6482c1859063105433c68fc1421abb
Sneha Menat <snehamenat029@gmail.com>
2024-03-27 21:41:28 +0530

    "changes in mongodb connections"

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://snehamenat029:sneha_029@cluster0.5domade.mongodb.net/")
+mongoose.connect("mongodb+srv://snehamenat029:sneha_029@cluster0.5domade.mongodb.net/ecommTest/test")
 .then(()=>{
     console.log("connection successfull...");
 }).catch((e)=>{
