98eaa87d67c57694704ec10d30b2b8dd8d30f919
Sneha Menat <snehamenat029@gmail.com>
2024-03-31 23:15:29 +0530

    "changes in backend"

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, auth-token"
     );
-    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE"); 
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
