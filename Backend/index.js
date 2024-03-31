d6a9f73c2c9ac795d481c540b2adddb4ae423fb2
Sneha Menat <snehamenat029@gmail.com>
2024-03-31 22:46:59 +0530

    "changes in authentication"

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+        "Origin, X-Requested-With, Content-Type, Accept, auth-token"
     );
     res.header("Access-Control-Allow-Methods", "POST, GET, DELETE");
     res.header("Access-Control-Allow-Credentials", "true");
